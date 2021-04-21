bf4e27317db1c411f7a3ab601847eaaab4ceb01f
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-04-21 20:29:39 -0300

    chore: update files

--- custom.d.ts
+++ custom.d.ts
@@ -2,3 +2,7 @@ declare module '*.png' {
   const content: any;
   export default any;
 }
+declare module "*.jpg" {
+  const content: any;
+  export default any;
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export * from './Button';
 export * from './Text';
 export * from "./TextInput";
 export * from "./Container";
+export * from "./Header";
+export * from "./Radio";
+export * from "./RadioGroup";
