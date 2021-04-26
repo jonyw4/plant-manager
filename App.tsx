985fa70768ef4c6615efb5af95a25bd100f7809d
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-04-25 23:48:29 -0300

    feat: inject dependancies in app root

--- App.tsx
+++ App.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from "@expo-google-fonts/jost";
 import { Routes } from './src/routes';
+import { ServicesContext } from './src/contexts';
+import { PlantServiceImpl, EnvironmentServiceImpl } from './src/infra';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Jost_400Regular, Jost_600SemiBold });
+  const plantService = new PlantServiceImpl();
+  const environmentService = new EnvironmentServiceImpl();
 
   if(!fontsLoaded){
     return (
       <AppLoading/>
     )
   }
-  return <Routes />;
+  return (
+    <ServicesContext.Provider value={{ plantService, environmentService }}>
+      <Routes />
+    </ServicesContext.Provider>
+  );
 }
