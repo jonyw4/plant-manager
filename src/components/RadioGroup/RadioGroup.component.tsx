13930fc9e31e5bfbe7afa3c07c453cc6fec7499a
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-04-27 00:24:52 -0300

    refactor: add key stractor to flatlist

--- src/components/RadioGroup/RadioGroup.component.tsx
+++ src/components/RadioGroup/RadioGroup.component.tsx
@@ -14,6 +14,7 @@ export function RadioGroup({ options, defaultValue, onChange }: RadioGroupProps)
     <RadioGroupContext.Provider value={state}>
       <FlatList
         data={options}
+        keyExtractor={(item) => String(item.value)}
         renderItem={({ item: option }) => (
           <Radio
             key={option.value}
