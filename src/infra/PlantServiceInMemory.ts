a3edf047333cababc6a1e510a47fdac68cc37749
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-04-27 23:45:30 -0300

    refacot: update plant service in memory

--- src/infra/PlantServiceInMemory.ts
+++ src/infra/PlantServiceInMemory.ts
@@ -3,6 +3,13 @@ import { Plant, TimeInterval, WaterTimeIntervals } from "../domain";
 import * as data from "./fakeData";
 
 export class PlantServiceInMemory implements PlantService {
+  async getPlant(id: number): Promise<Plant | undefined> {
+    const apiPlant = data.plants.find((plant) => plant.id === id)
+    if(!apiPlant){
+      return undefined;
+    }
+    return this.mapApiPlantToDomain(apiPlant);
+  }
   async getWaterTimeIntervals(): Promise<WaterTimeIntervals[]> {
     return data.plantsWaterFrequencies.map((frequency) => ({
       id: frequency.key as TimeInterval,
@@ -10,21 +17,27 @@ export class PlantServiceInMemory implements PlantService {
     }));
   }
   async getPlants(environment?: string): Promise<Plant[]> {
-    let plants: Plant[] = data.plants.map((plant) => ({
-      ...plant,
-      waterTips: plant.water_tips,
-      waterFrequency: {
-        value: plant.frequency.times,
-        interval: plant.frequency.repeat_every as TimeInterval,
-      },
-    }));
-    
-    if(environment){
-      plants = plants.filter((plant) => 
-        plant.environments.find(plantEnviroment => plantEnviroment === environment))
+    let plants: Plant[] = data.plants.map((plant) =>
+      this.mapApiPlantToDomain(plant)
+    );
+
+    if (environment) {
+      plants = plants.filter((plant) =>
+        plant.environments.find(
+          (plantEnviroment) => plantEnviroment === environment
+        )
+      );
     }
-    return plants
-      
+    return plants;
   }
-  
+  private mapApiPlantToDomain(apiPlant: typeof data.plants[number]): Plant {
+    return {
+      ...apiPlant,
+      waterTips: apiPlant.water_tips,
+      waterFrequency: {
+        value: apiPlant.frequency.times,
+        interval: apiPlant.frequency.repeat_every as TimeInterval,
+      },
+    };
+  }
 }
