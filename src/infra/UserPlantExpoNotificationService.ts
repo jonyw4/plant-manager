8cab05249554857742c8f28713c88f276ce5248e
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-04-28 23:08:20 -0300

    feat: add user plant expo notification service

---
+++ src/infra/UserPlantExpoNotificationService.ts
@@ -0,0 +1,32 @@
+import * as Notifications from "expo-notifications";
+import { UserPlantNotificationService } from "../data/UserPlantNotificationService";
+import { Plant } from "../domain";
+
+export class UserPlantExpoNotificationService implements UserPlantNotificationService {
+  async schedule(plant: Plant, notificationDate: Date): Promise<string> {
+    const nowDate = new Date();
+
+    const seconds = Math.abs(
+      Math.ceil((nowDate.getTime() - notificationDate.getTime()) / 1000)
+    );
+
+    return await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Heeey, 🌱",
+        body: `Está na hora de cuidar da sua ${plant.name}`,
+        sound: true,
+        priority: Notifications.AndroidNotificationPriority.HIGH,
+        data: {
+          plant,
+        },
+      },
+      trigger: {
+        seconds: seconds < 60 ? 60 : seconds,
+        repeats: true,
+      },
+    });
+  }
+  async cancelSchedule(notificationID: string): Promise<void> {
+    await Notifications.cancelScheduledNotificationAsync(notificationID);
+  }
+}
