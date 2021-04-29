767f5af27572bde63a9bd7c840eab8ad0211ee2b
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-04-28 23:05:52 -0300

    feat: add user plant notification service

---
+++ src/data/UserPlantNotificationService.ts
@@ -0,0 +1,6 @@
+import { Plant } from "../domain";
+
+export interface UserPlantNotificationService {
+  schedule(plant: Plant, notificationDate: Date): Promise<string>
+  cancelSchedule(notificationID: string): Promise<void>
+}
