1eed61b5aed98f3690aa295ea428c3594a52d408
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-04-28 23:06:05 -0300

    refactor: update use case context

--- src/contexts/UseCasesContext.ts
+++ src/contexts/UseCasesContext.ts
@@ -1,10 +1,13 @@
 import React from "react";
-import { EnvironmentService, PlantService } from "../data";
-import { GetUserPlants, SaveUser } from "../useCases";
+import { GetUserPlants, SaveUser, AddUserPlantToCurrentUser, RemoveUserPlantToCurrentUser } from "../useCases";
+import { GetUser } from "../useCases/GetUser";
 
 export interface UsesCasesContextData {
   getUserPlants: GetUserPlants;
   saveUser: SaveUser;
+  addUserPlantToCurrentUser: AddUserPlantToCurrentUser;
+  removeUserPlantToCurrentUser: RemoveUserPlantToCurrentUser;
+  getUser: GetUser;
 }
 
 // @ts-ignore
