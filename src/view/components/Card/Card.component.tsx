1143a96032353bbe22da954827cbfc33d5263c67
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-05-08 18:03:59 -0300

    refactor: update card component

--- src/view/components/Card/Card.component.tsx
+++ src/view/components/Card/Card.component.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { View } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { Text } from "../Text";
 import { CardProps } from "./Card.props";
